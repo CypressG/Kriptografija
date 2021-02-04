66b9f4d5f16accd08b52d87d41e71ae26ab926ed
CypressG <krasauskas.kiprass@gmail.com>
2021-02-04 12:10:14 +0200

    Fixed small tasks

---
+++ Python🐍/vigenere.py
@@ -0,0 +1,15 @@
+from abc import ABC
+import abc
+
+
+ABC_UPPER = 'ABCDEFGHIJKLMNOPQRTUVWXYZ'
+ABC_DOWN = 'abcdefghijklmnopqrstuvwxyz'
+
+
+class Vigenere:
+
+    def encryption(self, text):
+        pass
+
+    def decryption(self, text):
+        pass
