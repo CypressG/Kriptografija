4b9969d8a1560363b7276fcdc5ad18c8ce51e116
V01DF0G <V01DF0G@users.noreply.github.com>
2021-02-04 10:47:27 +0200

    First Stage

---
+++ CC++/premake5.lua
@@ -0,0 +1,49 @@
+workspace "Kriptografija"
+architecture "x86_64"
+startproject "CeaserCypherC++"
+
+configurations { "Debug", "Release" }
+    
+    flags
+	{
+		"MultiProcessorCompile"
+    }
+    
+    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "CeaserCypherC++"
+            location "CeaserCypherCPP"
+            kind "ConsoleApp"
+            language "C++"
+            cppdialect "C++17"
+            staticruntime "on"
+
+            targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+            objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+            files
+            {
+                "%{prj.name}/src/**.h",
+                "%{prj.name}/src/**.hpp",
+                "%{prj.name}/src/**.cpp"
+            }
+
+            defines
+            {
+                "_CRT_SECURE_NO_WARNINGS",
+            }
+
+            includedirs
+            {
+                "%{prj.name}/src"             
+            }
+
+            filter "configurations:Debug"
+                    defines { "DEBUG" }
+                    runtime "Debug"
+                    symbols "On"
+              
+                 filter "configurations:Release"
+                    defines { "NDEBUG" }
+                    runtime "Release"
+                    optimize "On"
