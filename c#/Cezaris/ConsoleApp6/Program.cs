dc9509cc6d612a1be5e973c7e5c3783fe3e50658
HellsinGas <65170199+HellsinGas@users.noreply.github.com>
2021-02-05 19:54:45 +0200

    Cezaris_nevalytas

---
+++ c#/Cezaris/ConsoleApp6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Hello World!");
+            Sifravimas naujas = new Sifravimas("Gaidys ąūžčč");
+
+            naujas.cezaris(naujas, 255);
+        }
+    }
+}
---
+++ c#/Cezaris/ConsoleApp6/Sifravimas.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp6
+{
+    public class Sifravimas
+    {
+        string Notencoded { get; set; }
+
+        string Cezencoded { get; set; }
+
+        byte[] Baitaienc { get; set; }
+
+        string Decoded { get; set; }
+
+        int CypherKey { get; set; }
+
+        public Sifravimas()
+        {
+
+        }
+
+        public Sifravimas(string motencoded)
+        {
+            Notencoded = motencoded;
+
+        }
+
+        public void cezaris(Sifravimas normtext, int key)
+        {
+            Queue<Byte> baitas = new Queue<Byte>();
+            byte[] bytas = System.Text.Encoding.UTF8.GetBytes(normtext.Notencoded);
+            byte suds = Convert.ToByte(key);
+            
+            foreach (byte c in bytas)
+            {
+                 baitas.Enqueue((byte)(c + suds));
+
+            }
+            foreach (byte c in baitas)
+            {
+                Console.WriteLine($"pirmas :{ c} ");
+            }
+
+            //foreach (byte c in baitas)
+            //  {
+            // normtext.Decoded = System.Text.Encoding.UTF8.GetString(baitas);
+            //}
+            bytas = baitas.ToArray();
+            normtext.Decoded = System.Text.Encoding.UTF8.GetString(bytas);
+            baitas.Clear();
+            foreach (byte c in baitas)
+            {
+
+            }
+            foreach (byte c in bytas)
+            {
+                baitas.Enqueue((byte)(c - suds));
+
+            }
+            bytas = baitas.ToArray();
+            normtext.Cezencoded = System.Text.Encoding.UTF8.GetString(bytas);
+            Console.WriteLine($", 213213213pawkepawodpasnjdmponas,,, {normtext.Decoded} ,,,,,,,,,,,,, {normtext.Cezencoded} ");
+
+        }
+
+    }
+}
