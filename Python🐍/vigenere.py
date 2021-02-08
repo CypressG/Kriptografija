786d60a16d281eb94918b5c7cfb0ee85551ec215
CypressG <krasauskas.kiprass@gmail.com>
2021-02-08 16:06:37 +0200

    Added vigenere cypher

--- Python🐍/vigenere.py
+++ Python🐍/vigenere.py
@@ -1,30 +1,58 @@
-from abc import ABC
-import abc
+import numpy
+from string import ascii_letters, ascii_uppercase, digits, punctuation, whitespace
+'''
+
+This task is done only using ASCII letters but the possibility of using foreign 
+letters is as well possible by refactoring some code and butting your own custom alphabet
+inside of array. 
+
+'''
 
 
-TEXT = "Hello World!"
+#CUSTOM_ALPHABET = "AĄBCČDEĘĖFGHIĮJKLMNOPQRŠTUŲŪVWXYZŽ"
 
+
+
+
+LETTERS = [x for x in range(65,91,1)]
+LETTERS_LENGTH = len(LETTERS)
+SYMBOLS = [x for x in range(33,48,1)]
+SYMBOLS_LENGTH = len(SYMBOLS)
+DIGITS = [x for x in range(48,58,1)]
+DIGITS_LENGTH = len(DIGITS)
+
+ALL_INCLUDED = 127 - 33
 
 class Vigenere:
 
     def __init__(self, plaintext, cipherkey):
-        self.plaintext = plaintext
-        self.cipherkey = cipherkey
+        self.plaintext = plaintext.upper()
+        self.cipherkey = cipherkey.upper()
+        self.extended()
 
-    def encryption(self, plaintext, cipherkey):
-        answer = ''
-        for x in plaintext:
+    def encryption(self):
+        self.answer = ''
+        for x in range(len(self.plaintext)):
+            value_letter = ord(self.plaintext[x])
+            key_letter = ord(self.cipherkey[x])
+            self.single_encryption_ascii(value_letter,key_letter,)
 
-    def range_ascii(self):
-        start = 1
-        for x in range(65, 91, 1):
-            print(f"{start} - {x} - {chr(x)}")
-            start += 1
+    def single_encryption_ascii(self,value,key,number_of_letters, position):
+        encrypted_letter = ((value + key) % number_of_letters) + position
+        return encrypted_letter
+
+    def single_encryption_custom(self,value,key,number_of_letters):
+        encrypted_letter = ((value + key) % number_of_letters)
+        return encrypted_letter
 
     def extended(self):
-        while(len(plaintext) > len(cipherkey)):
-            print()
+        counter = 0
+        while(len(self.plaintext) > len(self.cipherkey)):
+            self.cipherkey+=self.cipherkey[counter]
+            counter+=1
 
 
-krypto = Vigenere(plaintext=None, cipherkey=None)
-krypto.encryption("Kipras4123", "kipras")
+
+krypto = Vigenere(plaintext="Kipras4123", cipherkey="kipras123")
+
+#print(krypto.single_encryption_custom(0,16,len(CUSTOM_ALPHABET)))
