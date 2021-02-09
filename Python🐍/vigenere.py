565743241025b793096dbd58b4c3328dc738abc8
CypressG <krasauskas.kiprass@gmail.com>
2021-02-09 19:06:18 +0200

    Fixed case sensitivity, symbolsHOW TO USE IT?

--- Python🐍/vigenere.py
+++ Python🐍/vigenere.py
@@ -10,42 +10,47 @@ inside of array.
 #CUSTOM_ALPHABET = "AĄBCČDEĘĖFGHIĮJKLMNOPQRŠTUŲŪVWXYZŽ"
 
 ALL_INCLUDED = 122
+
+
 class Vigenere:
 
     def __init__(self, plaintext, cipherkey):
         self.plaintext = plaintext
         self.cipherkey = cipherkey
         self.extended()
+
     def encryption(self):
         self.encrypted = ''
         for x in range(len(self.plaintext)):
             self.encrypted += self.single_encryption_ascii(
-                ord(self.plaintext[x]),ord(self.cipherkey[x]), ALL_INCLUDED)
+                ord(self.plaintext[x]), ord(self.cipherkey[x]), ALL_INCLUDED)
         return print(self.encrypted)
-    def decryption(self,*args):
+
+    def decryption(self, *args):
         print(args)
         self.decrypted = ''
         if len(args) == 2:
             self.encrypted = args[0]
             self.extended(args[1])
         for x in range(len(self.encrypted)):
+            #If you would like to change decryption type (instead)
             self.decrypted += self.single_decryption_ascii(
-                ord(self.encrypted[x]), ord(self.cipherkey[x]), ALL_INCLUDED)
+                ord(self.encrypted[x]), ord(self.cipherkey[x]), ALL_INCLUDED) 
         return self.decrypted
 
-    def single_decryption_ascii(self, value, key, number_of_letters):
-        decrypted_letter = (value - key) % number_of_letters 
+    def single_decryption_unicode(self, value, key, number_of_letters):
+        decrypted_letter = (value - key) % number_of_letters
         return chr(decrypted_letter)
 
-    def single_encryption_ascii(self, value, key, number_of_letters):
-        encrypted_letter = (value + key ) % number_of_letters
+    def single_encryption_unicode(self, value, key, number_of_letters):
+        encrypted_letter = (value + key) % number_of_letters
         return chr(encrypted_letter)
 
     def single_encryption_custom(self, value, key, number_of_letters):
         encrypted_letter = (value + key) % number_of_letters
-        return encrypted_letter 
+        return encrypted_letter
 
-    def extended(self,*args):
+    def extended(self, *args):
         counter = 0
         if len(args) == 1:
             self.cipherkey = args[0]
@@ -57,4 +62,4 @@ class Vigenere:
 krypto = Vigenere(plaintext="attack", cipherkey="def")
 
 krypto.encryption()
-print(krypto.decryption("K_`KNW","def"))
+print(krypto.decryption("K_`KNW", "def"))
