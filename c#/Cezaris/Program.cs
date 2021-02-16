0274d2f371f1a41c81b470ea70fa68713c741050
HellsinGas <65170199+HellsinGas@users.noreply.github.com>
2021-02-16 15:58:44 +0200

    Added input

---
+++ c#/Cezaris/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            // Console.WriteLine("Hello World!");
+            Console.WriteLine("Input text you want to encrypt");
+            string a = Console.ReadLine();
+            Sifravimas naujas = new Sifravimas(a);
+            Console.WriteLine("Input shift");
+            string b = Console.ReadLine();
+            int c = Convert.ToInt32(b);
+
+            naujas.cezaris(naujas, c);
+        }
+    }
+}
