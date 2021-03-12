b515ea067a97b1e7f637e25e4ec964f4b72f5a52
HellsinGas <65170199+HellsinGas@users.noreply.github.com>
2021-03-12 11:05:49 +0200

    Cleaned up code

--- c#/AESencrypt/Form1.cs
+++ c#/AESencrypt/Form1.cs
@@ -16,9 +16,8 @@ namespace AESencrypt
     public partial class Form1 : Form
     {
         public static string paswordas;
-        public static string bloks = "oijasd21n";
-        public static bool mode;
-        //public static byte[] salt;
+        public static string bloks = "oijasd/21n964";
+        public static bool mode;        
         public Form1()
         {
             InitializeComponent();
@@ -28,7 +27,6 @@ namespace AESencrypt
         {
             OpenFileDialog op = new OpenFileDialog();
             op.DefaultExt = ".txt";
-           // bool? result = op.ShowDialog();
             if (op.ShowDialog() == DialogResult.OK)
             {
                 FileNameBox.Text = op.FileName;
@@ -38,9 +36,7 @@ namespace AESencrypt
                 }
 
             }
-        }
-        public byte[] salt = GenerateSalt();
-        public static RijndaelManaged aes = new RijndaelManaged();
+        }                
         public static string encryptedstring;
         private void EncryptButton_Click(object sender, EventArgs e)
         {
@@ -59,111 +55,11 @@ namespace AESencrypt
             fs.Close();
             MessageBox.Show(filetext);
             EncryptedTextBox.Text = AesEncry.AesEncryptas(filetext);
-           // FileStream fse = new FileStream(localLink, FileMode.Create);
-
             File.WriteAllText(localLink, EncryptedTextBox.Text);
            
-            //MessageBox.Show(bbz);
-           // string krva = AesEncry.AesDecryptas(bbz);
-           // MessageBox.Show(krva);
+           
 
 
-            /*//byte[] salt = GenerateSalt();
-            var localLink = FileNameBox.Text;//.Split('.')[0] + ".aes";
-            //FileStream fs = new FileStream(localLink, FileMode.Create);
-           // byte[] encryptedstring;
-           using(StreamReader k = new StreamReader(localLink))
-            {
-                encryptedstring = k.ReadToEnd();
-                MessageBox.Show(encryptedstring);
-            }
-            byte[] passwordBytes = Encoding.Unicode.GetBytes(PasswordBox.Text);
-            //  RijndaelManaged aes = new RijndaelManaged();
-           // FileStream fs = new FileStream(localLink, FileMode.Create);
-            aes.KeySize = 256;
-            aes.BlockSize = 128;
-            aes.Padding = PaddingMode.PKCS7;
-            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50);
-            aes.Key = key.GetBytes(aes.KeySize / 8);
-            aes.IV = key.GetBytes(aes.BlockSize / 8);
-            if (checkBox1.Checked)
-            {
-                aes.Mode = CipherMode.ECB;
-            }
-            else
-            {
-                aes.Mode = CipherMode.CBC;
-            }
-
-            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
-            byte[] encrypted;
-
-            // Create the streams used for encryption.
-            using (MemoryStream msEncrypt = new MemoryStream())
-            {
-                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
-                {
-                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
-                    {
-                        //Write all data to the stream.
-                        swEncrypt.Write(encryptedstring);
-                    }
-                    encrypted = msEncrypt.ToArray();
-                }
-            }
-            MessageBox.Show(encrypted.ToString());
-
-
-
-            *//* fs.Write(salt, 0, salt.Length);
-             CryptoStream cs = new CryptoStream(fs, aes.CreateEncryptor(), CryptoStreamMode.Write);
-             fs.Close();
-             FileStream fsIn = new FileStream(FileNameBox.Text, FileMode.Open);
-             byte[] buffer = new byte[108576];
-             int read;
-             try
-             {
-                 while((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
-                 {
-                     fsIn.Write(buffer, 0, read);
-                 }
-             }
-             catch(CryptographicException exc)
-             {
-                 MessageBox.Show($"{ exc}");
-             }
-             catch(Exception ex)
-             {
-                 MessageBox.Show(ex.Message);
-             }
-             finally
-             {
-                 fs.Close();
-                 fsIn.Close();
-                 using (StreamReader sr = new StreamReader(localLink))
-                 {
-                     EncryptedTextBox.Text = sr.ReadToEnd();
-                 }
-                 if (File.Exists(FileNameBox.Text))
-                 {
-                    // File.Delete(FileNameBox.Text);
-                    // FileNameBox.Text = String.Empty;
-                 }
-             }*/
-
-
-        }
-
-        public static byte[] GenerateSalt()
-        {
-            byte[] data = new byte[32];
-            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider()) {
-                for (int i = 0; i < 10; i++)
-                {
-                    rng.GetBytes(data);
-                }
-                return data;
-            }
         }
 
         private void Decrypt_Click(object sender, EventArgs e)
@@ -178,8 +74,7 @@ namespace AESencrypt
         private void button2_Click(object sender, EventArgs e)
         {
             OpenFileDialog op = new OpenFileDialog();
-            op.DefaultExt = ".txt";
-            // bool? result = op.ShowDialog();
+            op.DefaultExt = ".txt";            
             if (op.ShowDialog() == DialogResult.OK)
             {
                 FileNameBox.Text = op.FileName;
